5d0db88a09fe52842ffded20d20423e92f9886d3
Joao Laino <joaolainn@gmail.com>
2025-03-19 14:18:19 -0300

    commit

--- maiornumbers.py
+++ maiornumbers.py
@@ -1,15 +1,17 @@
 
+n1 = float(input("Qual o primeiro número: "))
+n2 = float(input("Qual o segundo número: "))
+
 def numbers(num1,num2):
     if num1 > num2:
         print(f"O {num1} é maior que o {num2}")
     elif num1 < num2:
         print(f"O {num2} é maior que o {num1}")
+    elif num1 == num2:
+        print(f"{n1} é igual a {n2}")
     else:
         print("Números não compreendidos")
 
-n1 = input("Qual o primeiro número: ")
-n2 = input("Qual o segundo número: ")
 resultado = numbers(n1,n2)
-print(resultado)
 
 
