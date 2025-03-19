f3bdd6fb8d1b13ae32462fc6d6c39cfde0bd5134
Joao Laino <joaolainn@gmail.com>
2025-03-19 13:08:25 -0300

    commit

--- lavanderia.py
+++ lavanderia.py
@@ -2,7 +2,7 @@ camisas = int(input("Digite o número de camisas: "))
 calcas = int(input("Digite o número de calças: "))
 meias = int(input("Digite o número de meias: "))
 
-valorcamisa = float(camisas*9.502)
+valorcamisa = float(camisas*9.50)
 valorcalca = float(calcas*15.00)
 valormeia = float(meias*1.20)
 total = valorcamisa+valorcalca+valormeia
