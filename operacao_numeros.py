1d4e70691248e4e0a2aa804e4ddb9ff2dead8c99
Joao Laino <joaolainn@gmail.com>
2025-03-19 14:30:06 -0300

    commit

--- operacao_numeros.py
+++ operacao_numeros.py
@@ -3,6 +3,7 @@ n1 = float(input("Qual o primeiro número: "))
 n2 = float(input("Qual o segundo número: "))
 operacao = input("Qual operação deseja fazer: ")
 
+#Relação entre os números
 def numbers(num1,num2):
     if num1 > num2:
         print(f"O {num1} é maior que o {num2}")
@@ -13,7 +14,7 @@ def numbers(num1,num2):
     else:
         print("Números não compreendidos")
 
-
+#Operação
 def operar(num1,num2):
     if operacao == "soma":
         soma = num1+num2
@@ -23,9 +24,8 @@ def operar(num1,num2):
         print(f"O valor da subtração é: {sub}")
     else:
         print("Erro na operação!")
-        
-    
 
+#Resultado
 numbers(n1,n2)
 operar(n1,n2)
 
